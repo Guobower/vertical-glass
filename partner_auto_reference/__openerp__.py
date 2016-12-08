dfe786962e5df692fb064e2aee9ae05ff9b25d7b
Valentin Thirion <vt@abakusitsolutions.eu>
2016-12-08 11:13:33 +0100

    [ADD] First working version

---
+++ partner_auto_reference/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Partner Auto Reference",
+    'version': '9.0.1.0.1',
+    'depends': [
+	   'base',
+    ],
+    'author': "AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Sale',
+    'description': 
+    """
+    This modules adds a method on the partners to auto set a
+    specific partner reference (based on a sequence) on the REFERENCE field.
+    
+    This module has been developed by AbAKUS it-solutions
+    """,
+    'data': [
+        'views/res_partner_view.xml',
+        ],
+}
---
+++ partner_auto_reference/models/res_partner.py
@@ -0,0 +1,14 @@
+from openerp import models, fields, api
+
+class res_partner_portal(models.Model):
+    _inherit = ['res.partner']
+    
+    ref_auto = fields.Char(string="Customer Reference", compute='_compute_customer_reference')
+
+    @api.multi
+    def _compute_customer_reference(self):
+        for partner in self:
+            if partner.parent_id:
+                partner.ref_auto = partner.parent_id.name.lower()[0:8].replace(" ", "") + "_" + partner.name.lower()[0:8].replace(" ", "")
+            else:
+                partner.ref_auto = partner.name.lower()[0:8].replace(" ", "")
