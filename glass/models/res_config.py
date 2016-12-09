fadea9f6e354228ba119f7735a785ee1cc994219
Valentin Thirion <vt@abakusitsolutions.eu>
2016-12-09 17:40:30 +0100

    [ADD] glass Set auto values on fields in SO

--- glass/models/res_config.py
+++ glass/models/res_config.py
@@ -14,6 +14,10 @@ class sale_glass_company_config_setting_check_config(models.Model):
     moving_product_id = fields.Many2one('product.product', 'Product for Moving')
     km_product_id = fields.Many2one('product.product', 'Product for KM')
 
+    installation_price = fields.Float('Installation Price', related='installation_product_id.list_price')
+    moving_price = fields.Float('Moving Price', related='moving_product_id.list_price')
+    km_price = fields.Float('KM Price', related='km_product_id.list_price')
+
 class sale_glass_company_config_settings(models.TransientModel):
     _inherit = 'res.config.settings'
     _name = 'glass.sale.config.settings'
@@ -40,6 +44,10 @@ class sale_glass_company_config_settings(models.TransientModel):
                 'installation_product_id': setting.installation_product_id.id,
                 'moving_product_id': setting.moving_product_id.id,
                 'km_product_id': setting.km_product_id.id,
+
+                'installation_price': setting.installation_price,
+                'moving_price': setting.moving_price,
+                'km_price': setting.km_price,
             }
 
     @api.one
@@ -52,3 +60,7 @@ class sale_glass_company_config_settings(models.TransientModel):
             setting.installation_product_id = self.installation_product_id
             setting.moving_product_id = self.moving_product_id
             setting.km_product_id = self.km_product_id
+
+            setting.installation_price = self.installation_price
+            setting.moving_price = self.moving_price
+            setting.km_price = self.km_price
--- glass/models/sale_order_line.py
+++ glass/models/sale_order_line.py
@@ -10,24 +10,53 @@ class SaleOrderLine(models.Model):
     sub_lines_total = fields.Float('Lines Total', compute='_compute_sub_lines_total', store=True)
 
     installation = fields.Boolean('Installation', default=False)
-    installation_qty = fields.Float('Installation Quantity')
-    installation_total = fields.Float('Installation Total')
+    installation_qty = fields.Float('Installation Quantity', default=1)
+    installation_total = fields.Float('Installation Total', compute='_compute_totals')
 
     moving = fields.Boolean('Moving', default=False)
     moving_qty = fields.Float('Moving Quantity')
-    moving_total = fields.Float('Moving Total')
+    moving_total = fields.Float('Moving Total', compute='_compute_totals')
 
     km_qty = fields.Float('KM Quantity')
-    km_total = fields.Float('KM Total')
+    km_total = fields.Float('KM Total', compute='_compute_totals')
 
-    # price_unit
+    price_unit = fields.Float('Price', compute='_compute_totals')
     margin_applied = fields.Float('Applied margin')
     # price_total
 
-    @api.depends('sub_lines_total')
+    @api.depends('sale_order_line_sub_ids')
     def _compute_sub_lines_total(self):
         for line in self:
             t = 0
             for sub_line in line.sale_order_line_sub_ids:
                 t = t + sub_line.total
             line.sub_lines_total = t
+
+    @api.model
+    @api.depends('installation', 'installation_qty', 'moving', 'moving_total', 'km_qty', 'sub_lines_total')
+    def _compute_totals(self):
+        setting = self.env['glass.sale.config.settings.data'].search([])
+        if len(setting) > 1:
+            setting = setting[0]
+
+        for line in self:
+            # installation total
+            if line.installation:
+                line.installation_total = line.installation_qty * setting.installation_price
+            else:
+                line.installation_total = 0
+
+            # moving total
+            if line.moving:
+                line.moving_total = line.moving_qty * setting.moving_price
+            else:
+                line.moving_total = 0
+
+            # KM total
+            line.km_total = line.km_qty * setting.km_price
+
+            # total without margin
+            line.price_unit = line.sub_lines_total + line.installation_total + line.moving_total + line.km_total
+
+            # total with margin
+            line.pric_total = line.price_unit * line.margin_applied
