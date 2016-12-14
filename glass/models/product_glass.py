40b18222ef940029a7180c6231b6b024e2d46024
Valentin Thirion <vt@abakusitsolutions.eu>
2016-12-14 15:30:10 +0100

    [ADD] glass Added marged price on products related to category

--- glass/models/product_glass.py
+++ glass/models/product_glass.py
@@ -6,3 +6,10 @@ class ProductGlass(models.Model):
     minimum_invoicable = fields.Float('Minimum Invoicable (m^2)', default=1)
     order_line_id = fields.Many2one('sale.order.line', 'Order Line')
     order_reference = fields.Char('Order reference')
+    price_with_auto_margin = fields.Float('Marged Price', compute='_compute_price_with_auto_margin')
+
+    @api.multi
+    @api.depends('categ_id')
+    def _compute_price_with_auto_margin(self):
+        for product in self:
+            product.price_with_auto_margin = product.lst_price * product.categ_id.margin_default
