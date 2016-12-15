1f7a13ebe5c15c3aaf360a44b33c6b9e4d65ab5f
Valentin Thirion <vt@abakusitsolutions.eu>
2016-12-15 15:55:59 +0100

    [ADD] glass Added field 'glass_supplier' on partners to only have them when selecting the supplier in SO

--- glass/models/res_partner.py
+++ glass/models/res_partner.py
@@ -4,10 +4,14 @@ class Partner(models.Model):
     _inherit = 'res.partner'
     
     ref_auto = fields.Char(string="Customer Reference", compute='_compute_customer_reference', store=True)
+    glass_supplier = fields.Boolean(string="Glass Supplier", default=False)
 
     @api.depends('name')
     def _compute_customer_reference(self):
         for partner in self:
-            names = partner.name.split(" ")
-            if len(names) >= 2:
-                partner.ref_auto = names[0][0:7].upper() + names[1][0:3].upper()
+            if partner.name:
+                names = partner.name.split(" ")
+                if len(names) >= 2:
+                    partner.ref_auto = names[0][0:7].upper() + names[1][0:3].upper()
+                else:
+                    partner.ref_auto = names[0][0:7].upper()
