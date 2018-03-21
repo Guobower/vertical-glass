9f6e23c772ced540517dbd979fc903229cdca49f
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-03-21 17:44:14 +0100

    [ADD] new fields to product.category

--- glass/models/product_category.py
+++ glass/models/product_category.py
@@ -1,6 +1,20 @@
-from openerp import models, fields, api
+# -*- coding: utf-8 -*-
+"""
+Product category
+"""
+
+import logging
+from openerp import api, fields, models
+_logger = logging.getLogger(__name__)
+
 
 class ProductCategory(models.Model):
+    """
+    Additional element to be inherited by glass product
+    """
     _inherit = 'product.category'
 
     margin_default = fields.Float('Default Margin', default=1)
+    min_invoice_area = fields.Integer(string="Min. Invoiceable Area [m^2]")
+    linked_product_ids = fields.One2many('product.product', 'categ_id', string="Linked Product")
+
