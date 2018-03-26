9752e13ed097eba0697043e56607e76a16b840a3
Paul Ntabuye Butera <pntabuye@gmail.com>
2018-03-26 17:29:31 +0200

    [FIX] sub_total computation and summary section

--- glass/models/product_glass_divider.py
+++ glass/models/product_glass_divider.py
@@ -17,7 +17,7 @@ class GlassDivider(models.Model):
 
     name = fields.Char(required=True)
     colour = fields.Char(required=True)
-    price = fields.Char('Price / m3', required=True)
+    price = fields.Float('Price / m^2', required=True)
 
     @api.multi
     def name_get(self):
@@ -26,5 +26,9 @@ class GlassDivider(models.Model):
         """
         result = []
         for record in self:
-            result.append((record.id, "%s (%s) [%s EUR]" % (record.name, record.colour, record.price)))
+            result.append((record.id, "%s (%s) [%.2f EUR]" % (record.name, record.colour, record.price)))
         return result
+
+    @api.multi
+    def compute_price(self, volume_in_cube_meters):
+        return self.price * volume_in_cube_meters
--- glass/models/sale_order_line_sub.py
+++ glass/models/sale_order_line_sub.py
@@ -31,38 +31,43 @@ class SaleOrderLineSub(models.Model):
     edge_width = fields.Selection([('0', '0'), ('1', '1'), ('2', '2')], 'Edges on W.', required=True, default=2)
     edge_height = fields.Selection([('0', '0'), ('1', '1'), ('2', '2')], 'Edges on H.', required=True, default=2)
 
-    perimeter = fields.Float('Perimeter (M)', compute='_compute_perimeter', store=True)
-    perimeter_total = fields.Float('Perimeter Total', compute='_compute_sub_totals')
+    perimeter = fields.Float('Perimeter (M)', compute='compute_perimeter', store=True)
+    perimeter_cost_price = fields.Float('Perimeter Cost Price', compute="compute_perimeter")
+    perimeter_total = fields.Float('Perimeter Total', compute='compute_perimeter')
 
-    area = fields.Float('Invoice Area (m^2)', compute='_computeArea', store=True)
-    area_geometric = fields.Float('Area (m^2)', compute='_computeArea', store=True)
-    area_cost_price = fields.Float('Area Cost Price (m^2)', compute="_set_product_info")
-    perimeter_cost_price = fields.Float('Perimeter Cost Price', compute="_set_product_info")
-    area_total = fields.Float('Area Total', compute='_compute_sub_totals')
+    area = fields.Float('Invoice Area (m^2)', compute='compute_area', store=True)
+    area_geometric = fields.Float('Area (m^2)', compute='compute_area', store=True)
+    area_total = fields.Float('Area Total', compute='compute_area')
+    area_cost_price = fields.Float('Area Cost Price (m^2)', compute="compute_area")
+
     minimum_invoiceable = fields.Float('Minimum Invoiceable (m^2)', related='glass_front_id.minimum_invoiceable')
-    area_max_exceeded = fields.Boolean('Max area exceeded for the glass', default=False, readonly=True, compute="_computeArea")
+
+    area_max_exceeded_front = fields.Boolean('Max area exceeded for the front glass', default=False, readonly=True, compute="compute_area")
+    area_max_exceeded_back = fields.Boolean('Max area exceeded for the back glass', default=False, readonly=True, compute="compute_area")
+    area_max_exceeded_middle = fields.Boolean('Max area exceeded for the middle glass', default=False, readonly=True, compute="compute_area")
+
     use_glass_substitute = fields.Boolean('Use Glass Substitute', default=False)
     # Options
     braces_id = fields.Many2one('product.glass.braces', 'Braces')
     divider_id = fields.Many2one('product.glass.divider', 'Divider')
     finish_id = fields.Many2one('product.glass.finish', 'Finish')
+    options_total = fields.Float('Total options', readonly=True, default=0)
     # Extras
     extras_ids = fields.Many2many('product.glass.extra', string="Extras")
+    extras_total = fields.Float('Total extras', readonly=True, default=0)
+    # Accessory
+    accessory_id = fields.Many2one('product.product', 'Accessory')
+    accessory_price = fields.Float('Acc. Price', default=0, compute="compute_accessory")
 
     multiplier = fields.Float('Multiplier', required=True, default=1.0)
-
     quantity = fields.Integer('Quantity', required=True, default=1)
-
-    accessory_id = fields.Many2one('product.product', 'Accessory')
-    accessory_price = fields.Float('Acc. Price', default=0, compute="_set_product_info")
     total = fields.Float('Total', compute="_compute_total", required=True)
 
+    # TBD
     supplier_id = fields.Many2one('res.partner', 'Supplier')
-
     standard_price = fields.Float(related='glass_front_id.standard_price')
     lst_price = fields.Float(related='glass_front_id.lst_price')
     margin = fields.Float(related='category_id.margin_default')
-
     dimension_constraint_id = fields.Many2one('product.glass.dimconstraint', 'Dimension constraint')
 
     @api.onchange('glass_front_id')
@@ -79,83 +84,119 @@ class SaleOrderLineSub(models.Model):
 
     @api.one
     @api.depends('glass_front_id', 'width', 'height')
-    def _computeArea(self):
+    def compute_area(self):
         """ Compute Area
         both geometric and invoiceable area
         """
-        # in order to have the area in square meters (mm * mm => m^2)
-        a = ((float(self.width) * float(self.height)) / 1000) / 1000
-        if self.glass_front_id and self.glass_front_id.minimum_invoiceable and self.glass_front_id.minimum_invoiceable > a:
-            a = self.glass_front_id.minimum_invoiceable
+        self.area = 0
+        self.area_total = 0
+        self.area_geometric = 0
+        self.area_cost_price = 0
+        unit_area = ((float(self.width) * float(self.height)) / 1000) / 1000
 
-        # Set area
-        self.area = a
+        if self.glass_front_id:
+            # in order to have the area in square meters (mm * mm => m^2)
+            a = unit_area
+            if self.glass_front_id.minimum_invoiceable and self.glass_front_id.minimum_invoiceable > unit_area:
+                a = self.glass_front_id.minimum_invoiceable
+                self.area += self.glass_front_id.minimum_invoiceable
+            else:
+                self.area += unit_area
+            # Set area - actually area of 1 item
+            self.area_geometric += unit_area
+            self.area_cost_price = self.glass_front_id.list_price
+
+            # Check if area is exceeded
+            if 0 < self.glass_front_id.maximum_area_possible < a:
+                self.area_max_exceeded_front = True
+            else:
+                self.area_max_exceeded_front = False
+
+        if self.glass_back_id:
+            # in order to have the area in square meters (mm * mm => m^2)
+            a = unit_area
+            if self.glass_back_id.minimum_invoiceable and self.glass_back_id.minimum_invoiceable > unit_area:
+                a = self.glass_back_id.minimum_invoiceable
+                self.area += self.glass_back_id.minimum_invoiceable
+            else:
+                self.area += unit_area
+            # Set area - actually area of 1 item
+            self.area_geometric += unit_area
+            self.area_cost_price += self.glass_back_id.list_price
+
+            # Check if area is exceeded
+            if 0 < self.glass_back_id.maximum_area_possible < a:
+                self.area_max_exceeded_back = True
+            else:
+                self.area_max_exceeded_back = False
+
+        if self.glass_middle_id:
+            # in order to have the area in square meters (mm * mm => m^2)
+            a = unit_area
+            if self.glass_middle_id.minimum_invoiceable and self.glass_middle_id.minimum_invoiceable > unit_area:
+                a = self.glass_middle_id.minimum_invoiceable
+                self.area += self.glass_middle_id.minimum_invoiceable
+            else:
+                self.area += unit_area
+            # Set area - actually area of 1 item
+            self.area_geometric += unit_area
+            self.area_cost_price += self.glass_middle_id.list_price
+
+            # Check if area is exceeded
+            if 0 < self.glass_middle_id.maximum_area_possible < a:
+                self.area_max_exceeded_middle = True
+            else:
+                self.area_max_exceeded_middle = False
 
         # Set price for area and shape
         if self.shape_id:
-            self.area_cost_price = self.glass_front_id.lst_price * float(self.shape_id.multiplier)
-        else:
-            self.area_cost_price = self.glass_front_id.lst_price
+            self.area_cost_price = self.area_cost_price*float(self.shape_id.multiplier)
 
-        # Check if area is exceeded
-        if 0 < self.glass_front_id.maximum_area_possible < self.area:
-            self.area_max_exceeded = True
-        else:
-            self.area_max_exceeded = False
+        self.area_total = self.area * self.area_cost_price
+
         self._compute_description()
 
-    # Compute Perimeter
     @api.one
     @api.depends('width', 'height', 'edge_width', 'edge_height')
-    def _compute_perimeter(self):
-        # In order to have the area in meters
-        p = ((float(self.width) * float(self.edge_width)) + (float(self.height) * float(self.edge_height))) / 1000
-        self.perimeter = p
+    def compute_perimeter(self):
+        """ In order to have the area perimeter in meters we divide by 1000
+        """
+        self.perimeter = ((float(self.width) * float(self.edge_width)) +
+                          (float(self.height) * float(self.edge_height))) / 1000
         if self.edge_id:
             self.perimeter_cost_price = float(self.edge_id.price)
-        self._compute_description()
-
-    # Set Product Related info
-    @api.one
-    @api.depends('glass_front_id', 'accessory_id', 'shape_id', 'edge_id')
-    def _set_product_info(self):
-        if self.type == 'glass':
-            if self.shape_id:
-                self.area_cost_price = self.glass_front_id.lst_price * float(self.shape_id.multiplier)
-            else:
-                self.area_cost_price = self.glass_front_id.lst_price
-            if self.edge_id:
-                self.perimeter_cost_price = float(self.edge_id.price)
-        if self.type == 'accessory':
-            self.accessory_price = self.accessory_id.lst_price
-            self.total = self.quantity * self.accessory_price * self.multiplier
-        self._compute_description()
-
-    # Compute base prices
-    @api.one
-    @api.depends('perimeter')
-    def _compute_base_price(self):
-        # Perimeter total
-        if self.edge_id:
-            self.perimeter_cost_price = float(self.edge_id.price)
-        else:
-            self.perimeter_cost_price = 0
-        self._compute_description()
-
-    # Compute sub-totals
-    @api.one
-    @api.depends('area', 'area_cost_price', 'perimeter_cost_price', 'finish_id')
-    def _compute_sub_totals(self):
-        self.area_total = self.area * self.area_cost_price
-        if self.finish_id and self.finish_id.price:
-            self.area_total = self.area_total + (self.area * self.finish_id.price)
         self.perimeter_total = self.perimeter * self.perimeter_cost_price
+
+        self._compute_description()
+
+    @api.one
+    @api.depends('accessory_id')
+    def compute_accessory(self):
+        self.accessory_price = self.accessory_id.lst_price
+        self._compute_description()
 
     @api.one
     @api.depends('quantity', 'area_total', 'perimeter_total', 'multiplier')
     def _compute_total(self):
         if self.type == 'glass':
-            self.total = self.quantity * (self.area_total + self.perimeter_total) * self.multiplier
+            # Add options
+            self.options_total = 0
+            if self.finish_id and self.finish_id.price:
+                self.options_total += self.finish_id.compute_price(self.area_geometric)
+            if self.divider_id and self.divider_id.price:
+                self.options_total += self.divider_id.compute_price(self.area_geometric)
+            if self.braces_id and self.braces_id.price:
+                self.options_total += self.braces_id.compute_price()
+            # Add Extras
+            self.extras_total = 0
+            if self.extras_ids:
+                for line in self.extras_ids:
+                    self.extras_total += line.price
+                self.extras_total = self.extras_total * self.area_geometric
+
+            self.total = self.options_total + self.extras_total + self.area_total + self.perimeter_total
+            # apply quantity and correction rate
+            self.total = self.quantity * self.area_total * self.multiplier
         if self.type == 'accessory':
             self.total = self.quantity * self.accessory_price * self.multiplier
         self._compute_description()
@@ -166,15 +207,26 @@ class SaleOrderLineSub(models.Model):
         if self.type == 'glass':
             text = ''
             if self.glass_front_id:
-                text = str(self.glass_front_id.categ_id.name.encode('utf-8')) + " - " + str(self.glass_front_id.name.encode('utf-8'))
+                text = "{} - {}".format(self.glass_front_id.categ_id.name.encode('utf-8'), self.glass_front_id.name.encode('utf-8'))
+            if self.glass_back_id:
+                text = "\n{} - {}".format(self.glass_back_id.categ_id.name.encode('utf-8'), self.glass_back_id.name.encode('utf-8'))
+            if self.glass_middle_id:
+                text = "\n{} - {}".format(self.glass_middle_id.categ_id.name.encode('utf-8'),self.glass_middle_id.name.encode('utf-8'))
             if self.quantity:
-                text = text + "\n- " + str(self.quantity) + " volume(s) de " + str(self.width) + "mm x " + str(self.height) + "mm - "
+                text = text + "\n- {} volume(s) de {} mm x {} mm -".format(self.quantity, self.width, self.height)
                 if self.shape_id:
                     text = text + str(self.shape_id.name.encode('utf-8'))
                 if self.finish_id:
                     text = text + ", " + str(self.finish_id.name.encode('utf-8'))
-            if self.area_max_exceeded:
+            if self.area_max_exceeded_front or self.area_max_exceeded_back or self.area_max_exceeded_middle:
                 setting = self.env['glass.sale.config.settings.data'].search([('company_id', '=', self.env.user.company_id.id)])
+                text = text + "\n /!\ "
+                if self.area_max_exceeded_front:
+                    text = "{}\n /!\ [{}]".format(text, self.glass_front_id.name.encode('utf-8'))
+                if self.area_max_exceeded_back:
+                    text = "{}\n /!\ [{}]".format(text, self.glass_back_id.name.encode('utf-8'))
+                if self.area_max_exceeded_middle:
+                    text = "{}\n /!\ [{}]".format(text, self.glass_middle_id.name.encode('utf-8'))
                 text = text + "\n /!\ " + str(setting.glass_maximum_area_warning.encode('utf-8'))
             if self.edge_id:
                 text = text + "\n- " + str(self.edge_id.name.encode('utf-8')) + " (" + str(self.edge_width) + " / " + str(self.edge_height) + ")"
