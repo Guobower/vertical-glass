ea82d9e97ccbc2477107836d6017319b4d0938a2
Valentin Thirion <vt@abakusitsolutions.eu>
2018-07-11 12:23:29 +0200

    test

--- glass/models/sale_order_line_sub.py
+++ glass/models/sale_order_line_sub.py
@@ -169,7 +169,7 @@ class SaleOrderLineSub(models.Model):
         ], order='rate desc')
         if len(rules) > 0:
             dim_constraint_rate += rules[0].rate/100
-            _logger.debug("\n\n\n TEST TEST : %s", rule)
+            _logger.debug("\n\n\n TEST TEST : %s", rules)
 
         self.area_cost_price = self.area_cost_price * dim_constraint_rate
 
